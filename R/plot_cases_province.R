11654093b64697f643303e5a17c36f73530494af
Lulu Pi <lpi2@student.monash.edu>
2020-10-23 03:28:53 +0800

    Added launch_app function

--- R/plot_cases_province.R
+++ R/plot_cases_province.R
@@ -6,22 +6,20 @@
 #' 
 #' @return A ggplot bar chart of COVID-19 with `type of cases` in three colors among all `provinces` on a specific day.
 #' 
-#' @import ggplot2
-#' 
-#' @examples 
-#' plot_cases_province(China_type)
+#' @importFrom magrittr %>% 
+#' @importFrom stats reorder
 #' 
 #' @export
 #'
 plot_cases_province <- function(data) {
   data %>%
-    ggplot2::ggplot(aes(x = reorder(`Province/State`, -Cases), y = Cases, fill = Type)) +
-    geom_bar(stat = "identity", position = "dodge") + 
-    theme_bw() + 
-    theme(plot.title = element_text(size = 20, color = "dark red"),
-          axis.title.x = element_text(size = 15, color = "dark blue"),
-          axis.title.y = element_text(size = 15, color = "dark blue"),
-          axis.text.x = element_text(angle = 90, color = "black"),
-          axis.line = element_line(color = "gray")) +
-    labs(x = "Province", y = "Cases", title = "COVID19 cases per day in China")
+    ggplot2::ggplot(ggplot2::aes(x = reorder(data$`Province/State`, -data$Cases), y = data$Cases, fill = data$Type)) +
+    ggplot2::geom_bar(stat = "identity", position = "dodge") + 
+    ggplot2::theme_bw() +
+    ggplot2::theme(plot.title = ggplot2::element_text(size = 20, color = "dark red"),
+                   axis.title.x = ggplot2::element_text(size = 15, color = "dark blue"),
+                   axis.title.y = ggplot2::element_text(size = 15, color = "dark blue"),
+                   axis.text.x = ggplot2::element_text(angle = 90, color = "black"),
+                   axis.line = ggplot2::element_line(color = "gray")) +
+    ggplot2::labs(x = "Province", y = "Cases", title = "COVID19 cases per day in China")
 }
