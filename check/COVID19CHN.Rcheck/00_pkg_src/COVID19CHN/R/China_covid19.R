1ecb3f1160c3fde041445062cdfd6fb913cea9ce
Lulu Pi <lpi2@student.monash.edu>
2020-10-23 06:15:43 +0800

    Fixed the check warnings

---
+++ check/COVID19CHN.Rcheck/00_pkg_src/COVID19CHN/R/China_covid19.R
@@ -0,0 +1,19 @@
+#' The situation of Coronavirus Cases in China
+#'
+#' This dataset contains the daily cases of covid-19 with all provinces in China from 1/22/2020 to 9/30/2020,
+#' including Confirmed, Deaths and Recovered cases.
+#' 
+#' @format the data set contains 8349 observations and 8 variables. 
+#' 
+#' - **Province/Stage**: The province of state in China
+#' - **Lat**: The latitude point of the province or state
+#' - **Long**: The longitude point of the province or state
+#' - **Date**: The date in formate of y/m/d
+#' - **Confirmed**: Daily confirmed cases
+#' - **Deaths**: Daily deaths cases
+#' - **Recovered**: Daily recovered cases
+#' - **Month**: The month of the day
+#' 
+#' @source [HDX](https://data.humdata.org/dataset/novel-coronavirus-2019-ncov-cases)
+"China_covid19"
+
